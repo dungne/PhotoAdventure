d3e1d84eadc37936476b66ee756f55440bfe6a97
jeon so yeun <s3622567@rmit.edu.vn>
2018-01-10 22:22:12 +0700

    sy part

--- app/src/main/java/com/photoadventure/photoadventure/ChangePhoto.java
+++ app/src/main/java/com/photoadventure/photoadventure/ChangePhoto.java
@@ -1,11 +1,13 @@
 package com.photoadventure.photoadventure;
 
 import android.*;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -43,10 +48,10 @@ public class ChangePhoto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_photo);
 
-        selectPhoto = findViewById(R.id.selectPhoto);
-        takePhoto = findViewById(R.id.takePhoto);
-        changePhoto = findViewById(R.id.changePhoto);
-        imageView = findViewById(R.id.imageView);
+        selectPhoto = (Button) findViewById(R.id.selectPhoto);
+        takePhoto = (Button) findViewById(R.id.takePhoto);
+        changePhoto = (Button) findViewById(R.id.changePhoto);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference();
@@ -58,7 +63,7 @@ public class ChangePhoto extends AppCompatActivity {
                 if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE);
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE);
                     }
                 } else {
                     gotoGallery();
@@ -79,6 +84,18 @@ public class ChangePhoto extends AppCompatActivity {
                 Intent intent = new Intent(ChangePhoto.this, MapsActivity.class);
                 Toast.makeText(getApplicationContext(), "Marker photo has been changed", Toast.LENGTH_LONG).show();
                 startActivity(intent);
+            }
+        });
+
+        storageReference.child("marker_photo").child("mPhoto").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(getApplicationContext()).load(uri).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(imageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                imageView.setImageResource(R.drawable.set_image);
             }
         });
     }
@@ -107,11 +124,7 @@ public class ChangePhoto extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Photo uploaded", Toast.LENGTH_LONG).show();
                 }
             });
-        }
-        else if (requestCode == CAMERA_INTENT && resultCode == RESULT_OK) {
-//            Bundle bundle = data.getExtras();
-//            Bitmap bitmap = (Bitmap) bundle.get("data");
-//            imageView.setImageBitmap(bitmap);
+        } else if (requestCode == CAMERA_INTENT && resultCode == RESULT_OK) {
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
             StorageReference filePath = storageReference.child("marker_photo").child("mPhoto");
